a67f330fc6754da3cb2a9caea9a1d327088a2663
DavidHerreros <david.herreroscalero@gmail.com>
2020-11-20 16:43:26 +0100

    Bug fixes and first implementation of card search

--- CopenMedViewer/popUp.js
+++ CopenMedViewer/popUp.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     $(".addCard").click(function() {
+        $(this).parents("#page-cell").find(".copenMed_container > div").each(function() {
+            var str = $(this).text().replace("done", "");
+            div = document.createElement('div');
+            $(div).attr({"id": "not-selected"});
+            $(div).css({"width": "100px"});
+            $(div).addClass("card").html(str).hide().fadeIn('slow');
+            $(".container-options").append(div);
+        });
+        $(this).parents("#page-cell").find(".user_selection > div").each(function() {
+            var str = $(this).text().replace("done", "");
+            div = document.createElement('div');;
+            $(div).attr({"id": "selected"});
+            $(div).css({"width": "100px", "border": "2px solid #3700B3"});
+            $(div).addClass("card").html(str).hide().fadeIn('slow');
+            $(".container-options").append(div);
+        });
         $("#modal-search").css({"display": "flex", "align-items": "center", "justify-content": "center"});
     });
 });
@@ -8,6 +24,10 @@ $(document).ready(function(){
     $(window).click(function(event) {
         if (event.target == $('#modal-search')[0]) {
             $('#modal-search').css("display", "none");
+            // $(".container-options").find("#selected").each(function(){
+
+            // });
+            $(".container-options").empty();
         }
     });
 });
@@ -15,6 +35,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".close").click(function() {
         $("#modal-search").css("display", "none");
+        $(".container-options").empty();
     });
 });
 
--- CopenMedViewer/searchCard.js
+++ CopenMedViewer/searchCard.js
@@ -1,31 +1,7 @@
-/* TODO: Change add cards to search cards in parent */
-
-var cardId = 1;
-
-$(document).ready(function(){
-    $("#addLabel").click(function(){
-        var str = $("#divInput").val();
-        var id = cardId;
-        div = document.createElement('div');
-        $(div).addClass("card").html(str).hide().fadeIn('slow');
-        $(div).attr({"id": "card-"+id});
-        // $(div).attr({"draggable": "true", "ondragstart": "dragStart(event)", "id": "card-"+id});
-        $("#userList").append(div);
-        $(".modal").css("display", "none");
-        cardId++;
+$("#searchCards").on("keyup", function() {
+    var key = this.value;
+    $(".tosearch").each(function() {
+       var $this = $(this);
+       $this.toggle($(this).text().indexOf(key) >= 0);
     });
-});
-
-$(document).on('keypress',function(e) {
-    if(e.which == 13 && $(".modal").css('display') == 'block') {
-        var str = $("#divInput").val();
-        var id = cardId;
-        div = document.createElement('div');
-        $(div).addClass("card").html(str).hide().fadeIn('slow');
-        $(div).attr({"id": "card-"+id});
-        // $(div).attr({"draggable": "true", "ondragstart": "dragStart(event)", "id": "card-"+id});
-        $("#userList").append(div);
-        $(".modal").css("display", "none");
-        cardId++;
-    }
 });
